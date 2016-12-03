bcc079389f9a29d014b3452659a8ded8d0a96243
joel gledhill <joel.gledhill@mail.utorono.ca>
2016-12-03 14:25:41 -0500

    login/signup connects to home page. make note and log out are implemented

--- js/note.js
+++ js/note.js
@@ -1,12 +1,37 @@
 var $ = jQuery;
 
 function DocumentReady(){
+
+	setAuthor();
+
+	function  setAuthor(){
+		$.ajax({
+			url : "/current",
+			method: "GET",
+			success : function(data){
+				if(data!==false){
+					$("#author").text(data);
+				}
+			}
+		});
+	}
+
 	document.getElementById("note-view").readOnly = true;
 	$("#toggle_edit").toggleClass("toggled");
 	
 	$("#toggle_edit").click(function() {
 		$(this).toggleClass("toggled");
 		toggleTextArea();
+	});
+
+	$('.logOut').click(function(){
+		$.ajax({
+			url : "/logout",
+			method: "GET",
+			success : function(data){
+				location.replace("http://localhost:3000/");
+			}
+		});
 	});
 
 	$('#save').click(function(){
@@ -24,6 +49,8 @@ function DocumentReady(){
 			success : function(data){
 				if (data === false){
 					alert("note with that title already exists by another user");
+				}else{
+					alert("changes have been saved");
 				}
 			}
 		});
